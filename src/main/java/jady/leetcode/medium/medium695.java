4c6f4c2f7107387f2ad94926b4ab25fa93e296ed
liuhan <hanliu@tujia.com>
2020-03-17 20:17:34 +0800

    udpate

---
+++ src/main/java/jady/leetcode/medium/medium695.java
@@ -0,0 +1,41 @@
+package jady.leetcode.medium;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by liuhan on 2020/3/15.
+ * https://leetcode-cn.com/problems/max-area-of-island/
+ */
+@Slf4j
+public class medium695 {
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{{0,0,1,0,0,0,0,1,0,0,0,0,0},{0,0,0,0,0,0,0,1,1,1,0,0,0},{0,1,1,0,1,0,0,0,0,0,0,0,0},{0,1,0,0,1,1,0,0,1,0,1,0,0},{0,1,0,0,1,1,0,0,1,1,1,0,0},{0,0,0,0,0,0,0,0,0,0,1,0,0},{0,0,0,0,0,0,0,1,1,1,0,0,0},{0,0,0,0,0,0,0,1,1,0,0,0,0}};
+        log.info("result = {}",maxAreaOfIsland(grid));
+    }
+
+    public static int maxAreaOfIsland(int[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    res = Math.max(res, dfs(i, j, grid));
+                }
+            }
+        }
+        return res;
+    }
+
+    private static int dfs(int i, int j, int[][] grid) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == 0) {
+            return 0;
+        }
+        grid[i][j] = 0;
+        int num = 1;
+        num += dfs(i + 1, j, grid);
+        num += dfs(i - 1, j, grid);
+        num += dfs(i, j + 1, grid);
+        num += dfs(i, j - 1, grid);
+        return num;
+
+    }
+}
